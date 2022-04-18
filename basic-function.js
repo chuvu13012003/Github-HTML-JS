e456445e506ad817623487b12c675a15a77f7616
chuvu13012003 <103091131+chuvu13012003@users.noreply.github.com>
2022-04-18 11:12:49 +0700

    Create basic-function.js

---
+++ basic-function.js
@@ -0,0 +1,6 @@
+var msg = 'Sign up to receive our newsletter for 10%!';
+function updateMessage() {
+    var el = document.getElementById(elemenntId 'message');
+    el.textContent = msg;
+}
+updateMessage();
